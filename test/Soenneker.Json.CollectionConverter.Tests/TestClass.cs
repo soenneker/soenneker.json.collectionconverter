360fd1c30cbc7780af5990f16b6a99795d3f4a88
Jake Soenneker <jake@soenneker.com>
2025-02-23 09:18:47 -0600

    Fixes enum

--- test/Soenneker.Json.CollectionConverter.Tests/TestClass.cs
+++ test/Soenneker.Json.CollectionConverter.Tests/TestClass.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Text.Json.Serialization;
-using Ardalis.SmartEnum.SystemTextJson;
 using Soenneker.Enums.DeployEnvironment;
 
 namespace Soenneker.Json.CollectionConverter.Tests;
@@ -8,6 +7,5 @@ namespace Soenneker.Json.CollectionConverter.Tests;
 public class TestClass
 {
     [JsonPropertyName("daysOfWeek")]
-    [System.Text.Json.Serialization.JsonConverter(typeof(CollectionConverter<SmartEnumNameConverter<DeployEnvironment, int>>))]
     public List<DeployEnvironment>? Environments { get; set; }
 }
